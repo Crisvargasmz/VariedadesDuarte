9345d34592a2385307caabb7db3b748708cadf43
Crisvargasmz <cristhiancesarvargas12@gmail.com>
2023-05-17 23:41:21 -0600

    Base de datos con sus procedimientos almacenados con modelo CRUD

--- Base_Datos.sql
+++ Base_Datos.sql
@@ -88,11 +88,11 @@ REFERENCES dbo.Cliente(IDCliente)
 
 
 ALTER TABLE dbo.Producto
-ADD IDCategaria  INT NOT NULL
+ADD IDCategoria  INT NOT NULL
 ALTER TABLE dbo.Producto
 ADD CONSTRAINT FK_Categoria_Producto
-FOREIGN KEY (IDCategaria)
-REFERENCES dbo.Categoria(IDCategaria)
+FOREIGN KEY (IDCategoria)
+REFERENCES dbo.Categoria(IDCategoria)
 
 ALTER TABLE dbo.Compra
 ADD IDProveedor  INT NOT NULL
@@ -242,86 +242,220 @@ CREATE PROCEDURE InsertarProducto
    GO
 -----------------------------------------------------------------------------------------------------
 
-   --procedimiento almacenado para consultar clientes
+   --procedimiento almacenado para consultar cliente
 
-   CREATE PROCEDURE ConsultarCliente
-  @nombre_cliente1 nvarchar(15),
-  @nombre_cliente2 nvarchar(15),
-  @apellido_cliente1 nvarchar(15),
-  @apellido_cliente2 nvarchar(15),
-  @telefono_cliente nvarchar(8),
-  @genero_cliente char (1) ,
-  @direccion_cliente nvarchar(150) 
+CREATE PROCEDURE ConsultarCliente
+    @nombre_cliente1 NVARCHAR(15) = NULL,
+    @nombre_cliente2 NVARCHAR(15) = NULL,
+    @apellido_cliente1 NVARCHAR(15) = NULL,
+    @apellido_cliente2 NVARCHAR(15) = NULL,
+    @telefono_cliente NVARCHAR(8) = NULL,
+    @genero_cliente CHAR(1) = NULL,
+    @direccion_cliente NVARCHAR(150) = NULL
+AS
+BEGIN
+    SELECT *
+    FROM Cliente
+    WHERE (nombre_cliente1 = @nombre_cliente1 OR @nombre_cliente1 IS NULL)
+        AND (nombre_cliente2 = @nombre_cliente2 OR @nombre_cliente2 IS NULL)
+        AND (apellido_cliente1 = @apellido_cliente1 OR @apellido_cliente1 IS NULL)
+        AND (apellido_cliente2 = @apellido_cliente2 OR @apellido_cliente2 IS NULL)
+        AND (telefono_cliente = @telefono_cliente OR @telefono_cliente IS NULL)
+        AND (genero_cliente = @genero_cliente OR @genero_cliente IS NULL)
+        AND (direccion_cliente = @direccion_cliente OR @direccion_cliente IS NULL);
+END;
 
-  AS
 
-
-  SELECT nombre_cliente1,nombre_cliente2,apellido_cliente1,apellido_cliente2,telefono_cliente,genero_cliente,direccion_cliente
-  FROM Cliente
-  --WHERE nombre_cliente1=@nombre_cliente1, nombre_cliente2=@nombre_cliente2,apellido_cliente1=@apellido_cliente1,apellido_cliente2=@apellido_cliente2,
-  --telefono_cliente=@telefono_cliente,genero_cliente=@genero_cliente,direccion_cliente=@direccion_cliente
-
-  GO
+GO
 
 --procedimiento almacenado para Consultar un proveedor
 CREATE PROCEDURE ConsultarProveedor
-  @nombre_proveedor1 NVARCHAR(15),
-  @nombre_proveedor2 NVARCHAR(15),
-  @apellido_proveedor1 NVARCHAR(15),
-  @apellido_proveedor2 NVARCHAR(15),
-  @empresa_proveedor NVARCHAR(30),
-  @telefono_proveedor NVARCHAR(8),
-  @direccion_proveedor NVARCHAR (150)
+    @nombre_proveedor1 NVARCHAR(15) = NULL,
+    @nombre_proveedor2 NVARCHAR(15) = NULL,
+    @apellido_proveedor1 NVARCHAR(15) = NULL,
+    @apellido_proveedor2 NVARCHAR(15) = NULL,
+    @empresa_proveedor NVARCHAR(30) = NULL,
+    @telefono_proveedor NVARCHAR(8) = NULL,
+    @direccion_proveedor NVARCHAR(150) = NULL
+AS
+BEGIN
+    SELECT *
+    FROM Proveedor
+    WHERE (nombre_proveedor1 = @nombre_proveedor1 OR @nombre_proveedor1 IS NULL)
+        AND (nombre_proveedor2 = @nombre_proveedor2 OR @nombre_proveedor2 IS NULL)
+        AND (apellido_proveedor1 = @apellido_proveedor1 OR @apellido_proveedor1 IS NULL)
+        AND (apellido_proveedor2 = @apellido_proveedor2 OR @apellido_proveedor2 IS NULL)
+        AND (empresa_proveedor = @empresa_proveedor OR @empresa_proveedor IS NULL)
+        AND (telefono_proveedor = @telefono_proveedor OR @telefono_proveedor IS NULL)
+        AND (direccion_proveedor = @direccion_proveedor OR @direccion_proveedor IS NULL);
+END;
 
-  AS
-
-  SELECT nombre_proveedor1,nombre_proveedor2, apellido_proveedor1, apellido_proveedor2, empresa_proveedor, telefono_proveedor, direccion_proveedor
-  FROM Proveedor
 
   GO
 
 --procedimiento almacenado para consultar un producto
 CREATE PROCEDURE ConsultarProducto
-  @nombre_producto NVARCHAR(40),
-  @cantidad_producto INTEGER ,
-  @precio_compra DECIMAL(12,2),
-  @precio_venta DECIMAL(12,2) ,
-  @descripcion NVARCHAR(200) ,
-  @fecha_vencimiento DATETIME
+    @nombre_producto NVARCHAR(40) = NULL,
+    @cantidad_producto INTEGER = NULL,
+    @precio_compra DECIMAL(12,2) = NULL,
+    @precio_venta DECIMAL(12,2) = NULL,
+    @descripcion NVARCHAR(200) = NULL,
+    @fecha_vencimiento DATETIME = NULL
+AS
+BEGIN
+    SELECT *
+    FROM Producto
+    WHERE (nombre_producto = @nombre_producto OR @nombre_producto IS NULL)
+        AND (cantidad_producto = @cantidad_producto OR @cantidad_producto IS NULL)
+        AND (precio_compra = @precio_compra OR @precio_compra IS NULL)
+        AND (precio_venta = @precio_venta OR @precio_venta IS NULL)
+        AND (descripcion = @descripcion OR @descripcion IS NULL)
+        AND (fecha_vencimiento = @fecha_vencimiento OR @fecha_vencimiento IS NULL);
+END;
 
-  AS
-
-  SELECT nombre_producto, cantidad_producto, precio_compra, precio_venta, descripcion, fecha_vencimiento
-  FROM Producto
 
   GO
 
    --procedimiento almacenado para consultar una categoria
-  CREATE PROCEDURE ConsultarCategoria
-  @nombre_categoria NVARCHAR (15)
+CREATE PROCEDURE ConsultarCategoria
+    @nombre_categoria NVARCHAR(15)
+AS
+BEGIN
+    SELECT *
+    FROM Categoria
+    WHERE nombre_categoria = @nombre_categoria;
+END;
 
-  AS
-
-  SELECT nombre_categoria
-  FROM Categoria
 
   GO
 
   --procedimiento almacenado para consultar una presentacion
 
-   CREATE PROCEDURE ConsultarPresentacion
-   @nombre_presentacion NVARCHAR (8)
+CREATE PROCEDURE ConsultarPresentacion
+    @nombre_presentacion NVARCHAR(8)
+AS
+BEGIN
+    SELECT *
+    FROM Presentacion
+    WHERE nombre_presentacion = @nombre_presentacion;
+END;
 
-   AS
-
-   SELECT nombre_presentacion
-   FROM Presentacion
 
    GO
 
    --------------------------------------------------------------------------------------------------
 
+ --Procedimiento almacenado para actualizar cliente
 
+ CREATE PROCEDURE ActualizarCliente
+    @IDCliente INTEGER,
+    @nombre_cliente1 NVARCHAR(15),
+    @nombre_cliente2 NVARCHAR(15),
+    @apellido_cliente1 NVARCHAR(15),
+    @apellido_cliente2 NVARCHAR(15),
+    @telefono_cliente NVARCHAR(8),
+    @genero_cliente CHAR(1),
+    @direccion_cliente NVARCHAR(150)
+AS
+BEGIN
+    UPDATE Cliente
+    SET nombre_cliente1 = @nombre_cliente1,
+        nombre_cliente2 = @nombre_cliente2,
+        apellido_cliente1 = @apellido_cliente1,
+        apellido_cliente2 = @apellido_cliente2,
+        telefono_cliente = @telefono_cliente,
+        genero_cliente = @genero_cliente,
+        direccion_cliente = @direccion_cliente
+    WHERE IDCliente = @IDCliente;
+END;
+
+GO
+
+ --Procedimiento almacenado para actualizar proveedor
+
+ CREATE PROCEDURE ActualizarProveedor
+    @IDProveedor INTEGER,
+    @nombre_proveedor1 NVARCHAR(15),
+    @nombre_proveedor2 NVARCHAR(15),
+    @apellido_proveedor1 NVARCHAR(15),
+    @apellido_proveedor2 NVARCHAR(15),
+    @empresa_proveedor NVARCHAR(30),
+    @telefono_proveedor NVARCHAR(8),
+    @direccion_proveedor NVARCHAR(150)
+
+AS
+BEGIN
+    UPDATE Proveedor
+    SET nombre_proveedor1 = @nombre_proveedor1,
+        nombre_proveedor2 = @nombre_proveedor2,
+        apellido_proveedor1 = @apellido_proveedor1,
+        apellido_proveedor2 = @apellido_proveedor2,
+        empresa_proveedor = @empresa_proveedor,
+        telefono_proveedor = @telefono_proveedor,
+        direccion_proveedor = @direccion_proveedor
+    WHERE IDProveedor = @IDProveedor;
+END;
+
+GO
+
+ --Procedimiento almacenado para actualizar producto
+
+CREATE PROCEDURE ActualizarProducto
+    @IDProducto INTEGER,
+    @nombre_producto NVARCHAR(40),
+    @cantidad_producto INT,
+    @precio_compra DECIMAL(12, 2),
+    @precio_venta DECIMAL(12, 2),
+    @descripcion NVARCHAR(200),
+    @fecha_vencimiento DATETIME
+   
+AS
+BEGIN
+    UPDATE Producto
+    SET nombre_producto = @nombre_producto,
+        cantidad_producto = @cantidad_producto,
+        precio_compra = @precio_compra,
+        precio_venta = @precio_venta,
+        descripcion = @descripcion,
+        fecha_vencimiento = @fecha_vencimiento
+    WHERE IDProducto = @IDProducto;
+END;
+
+GO
+
+ --Procedimiento almacenado para actualizar categoria
+
+CREATE PROCEDURE ActualizarCategoria
+    @IDCategoria INTEGER,
+    @nombre_categoria NVARCHAR(15)
+    
+AS
+BEGIN
+    UPDATE Categoria
+    SET nombre_categoria = @nombre_categoria
+    WHERE IDCategoria = @IDCategoria;
+END;
+
+GO
+
+ --Procedimiento almacenado para actualizar presentacion
+
+CREATE PROCEDURE ActualizarPresentacion
+
+@IDPresentacion INTEGER,
+@nombre_presentacion NVARCHAR (8)
+
+AS
+BEGIN
+     UPDATE Presentacion
+	 SET nombre_presentacion = @nombre_presentacion
+	 WHERE IDPresentacion=@IDPresentacion;
+	 END;
+
+GO
+
+
+   --------------------------------------------------------------------------------------------------
    ---Procedimiento almacenado para eliminar cliente
 
    CREATE PROCEDURE EliminarCliente
@@ -366,3 +500,18 @@ CREATE PROCEDURE EliminarProducto
   WHERE IDCategoria = @IDCategoria
 
   GO
+
+    --procedimiento almacenado para eliminar una presentacion
+
+CREATE PROCEDURE EliminarPresentacion
+@IDPresentacion INTEGER
+
+AS
+
+DELETE Presentacion
+WHERE IDPresentacion = @IDPresentacion
+
+GO
+
+
+
